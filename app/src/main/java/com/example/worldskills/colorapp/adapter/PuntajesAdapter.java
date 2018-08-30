f2759e51d0b4be02530e46b0cf6fdeeaeb0e71b8
WORLDSKILLS <victorsmanuels@outlook.es>
2018-08-30 11:50:51 -0500

    De victor para Diego

---
+++ app/src/main/java/com/example/worldskills/colorapp/adapter/PuntajesAdapter.java
@@ -0,0 +1,50 @@
+package com.example.worldskills.colorapp.adapter;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.worldskills.colorapp.R;
+import com.example.worldskills.colorapp.entidades.PuntajesVo;
+
+import java.util.ArrayList;
+
+public class PuntajesAdapter extends RecyclerView.Adapter<PuntajesAdapter.PuntajesHolder> {
+
+    public PuntajesAdapter(ArrayList<PuntajesVo> listaPuntajes) {
+        this.listaPuntajes = listaPuntajes;
+    }
+
+    ArrayList<PuntajesVo> listaPuntajes;
+
+    @Override
+    public PuntajesHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.modelo_puntajes_adapter, null, false);
+        return new PuntajesHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(PuntajesHolder holder, int position) {
+        holder.desplegadas.setText("Desplegadas: " + listaPuntajes.get(position).getDesplegadas());
+        holder.desplegadas.setText("Correctas: " + listaPuntajes.get(position).getCorrectas());
+        holder.desplegadas.setText("Incorrectas: " + listaPuntajes.get(position).getIncorrectas());
+    }
+
+    @Override
+    public int getItemCount() {
+        return listaPuntajes.size();
+    }
+
+    public class PuntajesHolder extends RecyclerView.ViewHolder {
+        TextView desplegadas, correctas, incorrectas;
+
+        public PuntajesHolder(View itemView) {
+            super(itemView);
+            desplegadas = itemView.findViewById(R.id.desplegadasModelo);
+            correctas = itemView.findViewById(R.id.correctasModelo);
+            incorrectas = itemView.findViewById(R.id.incorrectasModelo);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/worldskills/colorapp/entidades/Tipo.java
@@ -0,0 +1,8 @@
+package com.example.worldskills.colorapp.entidades;
+
+public class Tipo {
+    public static int tipo = 1;
+    public static int tiempoPartida = 0;
+    public static int intentos = 3;
+    public static int tiempoPalabra = 3;
+}
