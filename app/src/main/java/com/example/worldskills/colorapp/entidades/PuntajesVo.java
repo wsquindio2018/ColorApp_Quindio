b5c32d172b677dd06d72d76e2ac8616e913148da
WORLDSKILLS <victorsmanuels@outlook.es>
2018-08-30 11:31:25 -0500

    De victor para Diego

---
+++ app/src/main/java/com/example/worldskills/colorapp/entidades/PuntajesVo.java
@@ -0,0 +1,32 @@
+package com.example.worldskills.colorapp.entidades;
+
+public class PuntajesVo {
+
+    private int desplegadas;
+    private int correctas;
+    private int incorrectas;
+
+    public int getDesplegadas() {
+        return desplegadas;
+    }
+
+    public void setDesplegadas(int desplegadas) {
+        this.desplegadas = desplegadas;
+    }
+
+    public int getCorrectas() {
+        return correctas;
+    }
+
+    public void setCorrectas(int correctas) {
+        this.correctas = correctas;
+    }
+
+    public int getIncorrectas() {
+        return incorrectas;
+    }
+
+    public void setIncorrectas(int incorrectas) {
+        this.incorrectas = incorrectas;
+    }
+}
---
+++ app/src/main/java/com/example/worldskills/colorapp/utilidades/Utilidades.java
@@ -0,0 +1,13 @@
+package com.example.worldskills.colorapp.utilidades;
+
+public class Utilidades {
+
+    public static final String TABLA_PUNTAJES = "puntajes";
+    public static final String DESPLEGADAS = "desplegadas";
+    public static final String CORRECTAS = "correctas";
+    public static final String INCORRECTAS = "incorrectas";
+    public static final String CREAR_TABLA_PUNTAJES = "CREATE TABLE " + TABLA_PUNTAJES
+            + "( " + DESPLEGADAS + " INTEGER, "
+            + CORRECTAS + " INTEGER, "
+            + INCORRECTAS + " INTEGER)";
+}
